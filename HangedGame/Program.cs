c3ce822eaf3d251b6a88d0fbc5b6ff2a840a87f6
abigail <abigail.arte@gmail.com>
2023-10-18 18:47:37 +0200

    first commit

---
+++ HangedGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HangedGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe algo:");
+            try
+            {
+                string input = Console.ReadLine();
+                int a = int.Parse(input);
+                Console.WriteLine("Es " + a);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Lo que ingresaste no es un número válido.");
+            }
+
+            Console.WriteLine("Presiona Enter para salir...");
+            Console.ReadLine(); // Espera a que el usuario presione Enter antes de cerrar la consola
+        }
+    }
+}
